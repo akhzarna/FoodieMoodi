8e938ef23dd37153c73e3fae371b2b3afa855e41
akhzarna <link2akhzar@gmail.com>
2024-09-10 22:01:36 +0500

    Controls and Git and Array

---
+++ assets/Data/Food.tsx
@@ -0,0 +1,16 @@
+const Food = [
+    {
+      id:0,
+      title:'Pizza',
+      price:'14$',
+      image:'https://img.freepik.com/free-photo/slice-crispy-pizza-with-meat-cheese_140725-6974.jpg?w=900&t=st=1725901588~exp=1725902188~hmac=e0ad0adb5d6dfa5882d0e815855613b542837114cd90f98a03a320b9ebf773b1'
+    },
+    {
+      id:1,
+      title:'Burger',
+      price:'10$',
+      image:'https://img.freepik.com/free-photo/slice-crispy-pizza-with-meat-cheese_140725-6974.jpg?w=900&t=st=1725901588~exp=1725902188~hmac=e0ad0adb5d6dfa5882d0e815855613b542837114cd90f98a03a320b9ebf773b1'
+    }
+  ]
+
+  export default Food;
